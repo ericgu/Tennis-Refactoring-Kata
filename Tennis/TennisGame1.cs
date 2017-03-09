b3def0b855f16047a94297db25cf9b1fd8c8a1ac
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-03-09 07:57:22 -0800

    Duplicate block

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -82,12 +82,21 @@ namespace Tennis
 
                 return score;
             }
+            if (m_score1 >= 4 || m_score2 >= 4)
+            {
+                var minusResult = m_score1 - m_score2;
+                if (minusResult == 1) score = "Advantage player1";
+                else if (minusResult == -1) score = "Advantage player2";
+                else if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+
+                return score;
+            }
             return null;
         }
 
         private string EvenScore()
         {
-            string score;
             if (m_score1 == m_score2)
             {
                 if (m_score1 == 3)
