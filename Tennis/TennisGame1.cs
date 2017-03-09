cdb56688c30e3eb0282b2bcd4ca81264c473756c
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-03-09 07:58:47 -0800

    Split into advantage and win checks

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -71,23 +71,23 @@ namespace Tennis
 
         private string ScoreBiggerThan4()
         {
-            string score;
+            string score = null;
             if (m_score1 >= 4 || m_score2 >= 4)
             {
                 var minusResult = m_score1 - m_score2;
                 if (minusResult == 1) score = "Advantage player1";
                 else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
-                else score = "Win for player2";
 
-                return score;
+                if (score != null)
+                {
+                    return score;
+                }
             }
+
             if (m_score1 >= 4 || m_score2 >= 4)
             {
                 var minusResult = m_score1 - m_score2;
-                if (minusResult == 1) score = "Advantage player1";
-                else if (minusResult == -1) score = "Advantage player2";
-                else if (minusResult >= 2) score = "Win for player1";
+                if (minusResult >= 2) score = "Win for player1";
                 else score = "Win for player2";
 
                 return score;
