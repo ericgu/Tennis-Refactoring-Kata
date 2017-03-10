f8bf06b9e562c20992a52795040b4df1633c80bf
Eric Gunnerson <eric_gunnerson@hotmail.com>
2017-03-09 21:04:37 -0800

    Add some testes

---
+++ Tennis/ScoreRuleAdvantageTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework.Constraints;
+using NUnit.Framework;
+namespace Tennis
+{
+    [TestFixture]
+    internal class ScoreRuleAdvantageTests
+    {
+        [Test]
+        public void when_I_call_Evaluate_with_a_normal_score__it_returns_null()
+        {
+            Assert.IsNull(Setup(3, 3));
+        }
+
+        [Test]
+        public void when_I_call_Evaluate_with_a_winning_score__it_returns_null()
+        {
+            Assert.IsNull(Setup(5, 3));
+        }
+
+        [Test]
+        public void when_I_call_Evaluate_with_an_advantage__it_returns_correctly()
+        {
+            Assert.AreEqual("Advantage Fred", Setup(5, 4));
+        }
+
+        [Test]
+        public void when_I_call_Evaluate_with_an_advantage2__it_returns_correctly()
+        {
+            Assert.AreEqual("Advantage Barney", Setup(4, 5));
+        }
+
+        private static string Setup(int player1Score, int player2Score)
+        {
+            GameState gameState = new GameState("Fred", "Barney");
+
+            gameState.Player1.Score = player1Score;
+            gameState.Player2.Score = player2Score;
+
+            return new ScoreRuleAdvantage().Evaluate(gameState.Player1, gameState.Player2);
+        }
+    }
+}
--- Tennis/ScoreWords.cs
+++ Tennis/ScoreWords.cs
@@ -1,28 +1,12 @@
-using System;
-
 namespace Tennis
 {
     internal static class ScoreWords
     {
+        private static readonly string[] Words = {"Love", "Fifteen", "Thirty", "Forty"};
+
         public static string GetScoreWord(int tempScore)
         {
-            string word = String.Empty;
-            switch (tempScore)
-            {
-                case 0:
-                    word = "Love";
-                    break;
-                case 1:
-                    word = "Fifteen";
-                    break;
-                case 2:
-                    word = "Thirty";
-                    break;
-                case 3:
-                    word = "Forty";
-                    break;
-            }
-            return word;
+            return Words[tempScore];
         }
     }
 }
