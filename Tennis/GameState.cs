a661d6f636b258cf78092f2a7d8f1654641b4599
ericgu <eric_gunnerson@hotmail.com>
2017-03-09 17:11:39 -0800

    Cleanup

--- Tennis/GameState.cs
+++ Tennis/GameState.cs
@@ -2,20 +2,14 @@ namespace Tennis
 {
     class GameState
     {
-        public GameState(Player player1, Player player2)
-        {
-            Player1 = player1;
-            Player2 = player2;
-        }
-
         public Player Player1 { set; get; }
 
         public Player Player2 { set; get; }
 
         public GameState(string player1Name, string player2Name)
-            : this(new Player(player1Name), new Player(player2Name) )
         {
-            
+            Player1 = new Player(player1Name);
+            Player2 = new Player(player2Name);
         }
 
         public Player FindPlayerByName(string playerName)
