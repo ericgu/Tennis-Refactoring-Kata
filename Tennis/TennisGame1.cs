46006d2956f9dd821708951575527542d111267f
ericgu <eric_gunnerson@hotmail.com>
2017-03-09 16:30:46 -0800

    Cleanup names

--- Tennis/TennisGame1.cs
+++ Tennis/TennisGame1.cs
@@ -1,41 +1,38 @@
-using System;
-using System.Data.SqlClient;
-
 namespace Tennis
 {
     class TennisGame1 : ITennisGame
     {
-        private int m_score1 = 0;
-        private int m_score2 = 0;
-        private string player1Name;
-        private string player2Name;
-        private ScoreRuleChain _scoreRuleChain;
+        private int _mScore1;
+        private int _mScore2;
+        private readonly string _player1Name;
+        private string _player2Name;
+        private readonly ScoreRuleChain _scoreRuleChain;
 
         public TennisGame1(string player1Name, string player2Name)
         {
-            this.player1Name = player1Name;
-            this.player2Name = player2Name;
+            _player1Name = player1Name;
+            _player2Name = player2Name;
 
             _scoreRuleChain = new ScoreRuleChain(new IScoreRule[]
             {
                 new ScoreRuleEven(),
-                new ScoreRuleAdvantage("player1", "player2"),
-                new ScoreRuleWin("player1", "player2"),
+                new ScoreRuleAdvantage(player1Name, player2Name),
+                new ScoreRuleWin(player1Name, player2Name),
                 new ScoreRuleNormal()
             });
         }
 
         public void WonPoint(string playerName)
         {
-            if (playerName == "player1")
-                m_score1 += 1;
+            if (playerName == _player1Name)
+                _mScore1 += 1;
             else
-                m_score2 += 1;
+                _mScore2 += 1;
         }
 
         public string GetScore()
         {
-            return _scoreRuleChain.Evaluate(m_score2, m_score1);
+            return _scoreRuleChain.Evaluate(_mScore2, _mScore1);
         }
     }
 }
